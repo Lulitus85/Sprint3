88421c007ef91d95ab4050942a91cb9659f62218
Lucia Cavagna <cavagnalucia@gmail.com>
2019-05-19 23:12:24 -0300

    estamos

--- includes/__nav_logged.php
+++ includes/__nav_logged.php
@@ -5,10 +5,19 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="profile.php"> Bienvenid@ <?=$_SESSION["name"];?> </a>
+          <ul class="navbar-nav cascadaUno">
+            
+            <li class="nav-item dropdown">
+              <a class="nav-link usuario dropdown-toggle" data-toggle="dropdown" href="profile.php"> <?=$_SESSION["name"];?>  </a>
+              <ul class="dropdown-menu cascadaDos">
+                <li><a href="#">Editar Perfil</a></li>
+                <li class="borderli"><a href="#">Configuración</a></li>
+                <li><a href="login.php">Cerrar Sesión
+                </a></li>
+              </ul>
+
             </li>
             <li class="nav-item">
               <a class="nav-link" href="faq.php">FAQ</a>
--- profile.php
+++ profile.php
@@ -21,38 +21,44 @@ $user = $_SESSION['email'];
 }
  ?>
 
-<!-- AQUI TERMINA EL NAV -->
+  <!-- AQUI TERMINA EL NAV -->
 
-<!--CUERPO DE BIENVENIDA-->
+  <!--CUERPO DE BIENVENIDA-->
 
-<div class="contenedorPerfil">
+  <div class="contenedorPerfil">
 
-  <div class="card">
+    <div class="card">
 
-    <div class="imagen">
-      <img class="card-img-top" src="imagenesUsuarios/<?=$_SESSION["avatar"];?>" alt="foto usuario">
-      <!--aca embebo la foto de perfil del usuario en la carta-->
+      <div class="imagen">
+        <img class="card-img-top" src="imagenesUsuarios/<?=$_SESSION["avatar"];?>" alt="foto usuario">
+        <!--aca embebo la foto de perfil del usuario en la carta-->
+      </div>
+
+      <div class="card-body">
+        <p class="card-text"><?=$_SESSION["name"];?></p>
+      </div>
+
     </div>
-
-    <div class="card-body">
-      <p class="card-text"><?=$_SESSION["name"];?></p>
-      <h6><a href="logout.php">Cerrar sesion</a></h6> <!-- aca es el link de cerrar sesion -->
-    </div>
-
   </div>
-</div>
 
-<!--FIN CUERPO BIENVENIDA-->
+  <!--FIN CUERPO BIENVENIDA-->
 
-<!-- AQUI EMPIEZA EL FOOTER -->
+  <!-- AQUI EMPIEZA EL FOOTER -->
 
-<?php include_once 'includes/__footer.php'; ?>
+  <?php include_once 'includes/__footer.php'; ?>
 
   <!-- AQUI TERMINA EL FOOTER -->
 
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+  </script>
 
 </body>
+
 </html>
