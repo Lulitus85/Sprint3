a2dae35d694b1800b144ba29d8eeafa3e1ef7fd3
mboudakian <michelle.boudakian@gmail.com>
2019-05-29 00:21:53 -0300

    cajas de productos

--- profile.php
+++ profile.php
@@ -39,6 +39,22 @@ $user = $_SESSION['email'];
       </div>
 
     </div>
+    <div class="caja-productos">
+  <section class="productos-perfil">
+    
+    <article class="producto-individual">
+      <img class="imagen-producto" src="imagenesUsuarios/5cec1f0f6f3b7.jpg" alt="imagen de producto">
+      <a href="#"><img class="solicitar" src="img/solicitar_-01.svg" alt="solicitar"></a>
+      <h4 class="nombre-categoria">Lectura </h4>
+      <h5 class="nombre-subcategoria">Subcategoria: </h5>
+      <h5 class="nombre-producto">Producto: </h5>
+      <a href="#"><h5 class="ver-fotos">VER FOTOS</h5></a>
+    </article>
+   
+    
+  </section>
+  </div>
+
   </div>
 
   <!--FIN CUERPO BIENVENIDA-->
