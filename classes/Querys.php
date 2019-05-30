687158e43e604ad61d7efe47c8484c0a2c2f39a2
DigitalHouse <cavagnalucia@gmail.com>
2019-05-29 21:58:59 -0300

    pdo

---
+++ classes/Querys.php
@@ -0,0 +1,9 @@
+<?php
+
+class Querys{
+    static public function saveUser(User $user, $tabla, $pdo)
+    //le voy a pasar el objeto usuario, la tabla donde quiero guardarlo y la conexión a la base de datos que corresponde
+    {
+        INSERT INTO $tabla
+    }
+}
--- loader.php
+++ loader.php
@@ -6,15 +6,27 @@ require 'classes/User.php';
 require 'classes/Validator.php';
 require 'classes/RegisterFile.php';
 require 'classes/Database.php';
-require 'classes/Json.php';
+//require 'classes/Json.php';
 require 'classes/Auth.php';
+require 'classes/MySql.php';
+require 'classes/Querys.php';
+
+//Declaro mis variables
+$host = "localhost";
+$bd = "vintrash_db";
+$usuario = "root";
+$password = "root";
+$puerto = "3306";
+$charset = "utf8mb4";
+//Ojo: Para los que trabajan con MAC: deben colocar el puerto: 8889
+
+$pdo = BaseMySql::conexion($host,$bd,$usuario,$password,$puerto,$charset);
 
 session_start();
 
 $validator = new Validator();
 $register = new RegisterFile();
-$intenso = new Json('usuario.json');
+//$intenso = new Json('usuario.json');
 $auth = new Auth();
-
 
 ?>
