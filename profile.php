bb3bbcc0692a0c25b78824dbcf5bae674dcb6fd1
Lucia Cavagna <cavagnalucia@gmail.com>
2019-06-20 16:50:58 -0300

    javaScript Profile

--- profile.php
+++ profile.php
@@ -30,9 +30,10 @@ $user = $_SESSION['email'];
     <div class="card">
 
       <div class="imagen">
-        <img class="card-img-top" src="imagenesUsuarios/<?=$_SESSION["avatar"];?>" alt="foto usuario">
-        <!--aca embebo la foto de perfil del usuario en la carta-->
-      </div>
+        <img class="card-img-top" src="imagenesUsuarios/<?= $_SESSION["avatar"];?>" alt="foto usuario">
+
+  <!--aca embebo la foto de perfil del usuario en la carta-->
+        </div>
 
       <div class="card-body">
         <p class="card-text"><?=$_SESSION["name"];?></p>
@@ -40,15 +41,55 @@ $user = $_SESSION['email'];
 
     </div>
   </div>
+  <br>
+  <br>
+  <br>
 
   <!--FIN CUERPO BIENVENIDA-->
+  <div class="caja-productos">
 
+    <section class="productos-perfil">
+
+      <article class="producto-individual">
+        <div class="producto">
+          <img class="imagen-producto" src="imagenesUsuarios/Productos/music.jpg" alt="imagen de producto">
+        </div>
+        <div class="boton">
+          <a href="#"><img class="solicitar" src="img/solicitar_-01.svg" alt="solicitar"></a>
+        </div>
+        <div class="info">
+          <h4 class="nombre-categoria">Lectura </h4>
+          <h5 class="nombre-subcategoria">Subcategoria: Comics</h5>
+          <h5 class="nombre-producto">Producto: Superman N°1</h5>
+          <a href="#" id="abrir">
+            <h5 class="ver-fotos">VER FOTOS</h5>
+          </a>
+        </div>
+      </article>
+
+     
+      <div id="miModal" class="modalito">
+          <div id="flex" class="flex">
+            <div class="contenido_modal">
+                <span id="close" class="close"></span>
+<!--                 acá va un carrousel con las fotos del producto -->
+                <img src="imagenesUsuarios/Productos/music.jpg" alt="">
+            </div>
+          </div>
+      </div>     
+    
+    </section>
+
+  </div>
+
+
+  </div>
   <!-- AQUI EMPIEZA EL FOOTER -->
 
   <?php include_once 'includes/__footer.php'; ?>
 
   <!-- AQUI TERMINA EL FOOTER -->
-
+  <script src="modal.js" ></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
     integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
   </script>
